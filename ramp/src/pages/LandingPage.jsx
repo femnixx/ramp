3223237bcbeda65048dc5fc7c152c374e7f04be8
femnix <femnitality@gmail.com>
2025-07-11 20:59:44 +0700

    Got tailwind to start working

---
+++ ramp/src/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+import "../App.css";
+import React from "react";
+
+const LandingPage = () => {
+  return <div className="text-black bg-green-800">LandingPage</div>;
+};
+
+export default LandingPage;
